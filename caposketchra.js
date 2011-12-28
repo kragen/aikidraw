0a10d4d535a051f049b5b67ab2c7a04af3f058a7
kragen <kragen@VOSTRO9.(none)>
2011-12-27 23:07:38 -0300

    put caposketchra code into a single namespace

--- caposketchra.js
+++ caposketchra.js
@@ -2,65 +2,68 @@
 // D draw lines with mouse
 // D fix it so it works in Firefox
 // D store lines in localStorage
-// - namespace everything
+// D namespace everything
 // - add multiple colors
 // - add multiple thicknesses
 // - make localStorage memory-efficient
 // - make localStorage linear-time
 
-$(setup) // for some reason Firefox can't getElementById before DOMContentLoaded
+var capo = { mousePos: null
+           , cx: null
+           , offsetTop: null
+           , offsetLeft: null
+           , drawing: []
 
-var mousePos = null
-  , cx = null
-  , offsetTop = null
-  , offsetLeft = null
-  , drawing = []
+           , setup: function() {
+               var cv = $('#c')
+                 , offset = cv.offset()
 
-function setup() {
-  var cv = $('#c')
-    , offset = cv.offset()
+               capo.cx = cv[0].getContext('2d')
+               capo.offsetTop = offset.top
+               capo.offsetLeft = offset.left
 
-  cx = cv[0].getContext('2d')
-  offsetTop = offset.top
-  offsetLeft = offset.left
+               cv
+               .mousedown(function(ev) { capo.mousePos = capo.evPos(ev) })
+               .mouseup(function() { capo.mousePos = null })
+               .mousemove(capo.mouseMoveHandler)
 
-  cv
-  .mousedown(function(ev) { mousePos = evPos(ev) })
-  .mouseup(function() { mousePos = null })
-  .mousemove(mouseMoveHandler)
+               capo.cx.strokeStyle = '1px solid black'
 
-  cx.strokeStyle = '1px solid black'
+               var drawing = localStorage.getItem('currentDrawing')
+               if (drawing) {
+                 capo.drawing = JSON.parse(drawing)
+                 capo.drawing.map(function(line) {
+                   capo.drawLine(line[0], line[1])
+                 })
+               }
+             }
 
-  if (localStorage.getItem('currentDrawing')) {
-    drawing = JSON.parse(localStorage.getItem('currentDrawing'))
-    drawing.map(function(line) {
-      drawLine(line[0], line[1])
-    })
-  }
-}
+           , mouseMoveHandler: function(ev) {
+               if (capo.mousePos === null) {
+                 return
+               }
 
-function mouseMoveHandler(ev) {
-  if (mousePos === null) {
-    return
-  }
+               var newPos = capo.evPos(ev)
+               capo.addLine(capo.mousePos, newPos)
+               capo.drawLine(capo.mousePos, newPos)
+               capo.mousePos = newPos
+             }
 
-  var newPos = evPos(ev)
-  addLine(mousePos, newPos)
-  drawLine(mousePos, newPos)
-  mousePos = newPos
-}
+           , drawLine: function(oldPos, newPos) {
+               var cx = capo.cx
+               cx.moveTo(oldPos[0], oldPos[1])
+               cx.lineTo(newPos[0], newPos[1])
+               cx.stroke()
+             }
 
-function drawLine(oldPos, newPos) {
-  cx.moveTo(oldPos[0], oldPos[1])
-  cx.lineTo(newPos[0], newPos[1])
-  cx.stroke()
-}
+           , addLine: function(oldPos, newPos) {
+               capo.drawing.push([oldPos, newPos])
+               localStorage.setItem('currentDrawing', JSON.stringify(capo.drawing))
+             }
 
-function addLine(oldPos, newPos) {
-  drawing.push([oldPos, newPos])
-  localStorage.setItem('currentDrawing', JSON.stringify(drawing))
-}
+           , evPos: function(ev) {
+               return [ev.pageX - capo.offsetLeft, ev.pageY - capo.offsetTop]
+             }
+           }
 
-function evPos(ev) {
-  return [ev.pageX - offsetLeft, ev.pageY - offsetTop]
-}
+$(capo.setup) // for some reason Firefox can't getElementById before DOMContentLoaded
